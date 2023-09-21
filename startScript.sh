1d769ff0c365191503e5878e5de6c8cca3264d14
redguy212 <105039198+redguy212@users.noreply.github.com>
2023-09-21 11:29:34 +1000

    Update startScript.sh
    universal - have to insert path

--- startScript.sh
+++ startScript.sh
@@ -2,4 +2,4 @@
 # Started from /etc/xdg/LXDE-pi/autostart
 echo "Starting timelapse program in 10 seconds."
 sleep 10
-lxterminal --working-directory='/home/timelapse1/timelapse' --command='python3 timelapseImageFinal.py'
+lxterminal --working-directory='[where youre code is saved]' --command='python3 timelapseImageFinal.py'
